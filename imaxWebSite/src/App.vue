6d3ddfdabc3fc40ac164b3b60c084dbbaaac8e42
Jonatan1991 <aguilayony@gmail.com>
2026-06-16 14:30:49 +0200

    feat: add header component and Vue directive examples for v-for, v-if, v-bind, and reactivity

--- imaxWebSite/src/App.vue
+++ imaxWebSite/src/App.vue
@@ -1,24 +1,31 @@
 <script setup>
 import VariableReactiva from "./components/ejemplos/variableReactiva.vue";
+import VariableReactiva2 from "./components/ejemplos/variableReactiva2.vue";
+import VBind from "./components/ejemplos/vBind.vue";
+import VFor from "./components/ejemplos/vFor.vue";
 import headerComponent from "./components/headerComponent.vue";
 </script>
 
 <template>
-  <header>
+  <header class="header">
     <headerComponent></headerComponent>
   </header>
 
-  <body>
+  <body class="body">
     <h1 class="title">Hello Imax Web</h1>
-    <VariableReactiva></VariableReactiva>
 
+    <VariableReactiva></VariableReactiva>
+    <hr>
+    <VariableReactiva2></VariableReactiva2>
+    <hr>
+    <VBind></vbind>
+    <hr>
+    <VFor></vFor>
   </body>
-  <footer>
+  <footer class="footer">
     <p>footer</p>
   </footer>
 </template>
-
-
 
 <style>
 .title {
---
+++ imaxWebSite/src/components/ejemplos/vFor.vue
@@ -0,0 +1,29 @@
+<script setup>
+const movies = [
+    {
+        id: 1,
+        name: "The Matrix",
+        year: 1999
+    },
+    {
+        id: 2,
+        name: "Jurassic Park",
+        year: 1993
+    },
+    {
+        id: 3,
+        name: "The Lion King",
+        year: 1994
+    }
+]
+
+</script>
+<template>
+    <h2>Directiva v-for</h2>
+    <ul>
+        <li v-for="movie in movies" :key="movie.id">
+            <p>Name: {{ movie.name }} - Year: {{ movie.year }}</p>
+        </li>
+    </ul>
+</template>
+<style></style>
