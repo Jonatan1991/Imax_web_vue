c9b665f788223186e732d168cb0780d29dcc91ba
Jonatan1991 <75802929+Jonatan1991@users.noreply.github.com>
2026-06-27 08:34:05 +0200

    feat: add reusable ChildComponent and login components to the component library

---
+++ src/components/childComponents/ChildComponents.vue
@@ -0,0 +1,24 @@
+<script setup>
+defineProps({
+    msg: {
+        type: String,
+        required: true
+    },
+    person: {
+        name: String,
+        age: Number,
+        email: String,
+        required: true
+    }
+})
+</script>
+<template>
+    <h2>Este es un componente hijo</h2>
+    <p>Mensaje: {{ msg }}</p>
+    <p>Name: {{ person.name }}</p>
+    <p>Age: {{ person.age }}</p>
+    <p>Email: {{ person.email }}</p>
+</template>
+
+<style>
+</style>
---
+++ src/components/childComponents/login.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { ref } from "vue";
+
+const emit = defineEmits(["login"])
+
+const user = ref("Adrina");
+const password = ref("1234");
+
+const login = () => {
+    emit("login", user.value)
+}
+</script>
+<template>
+    <div class="login-container">
+        <h2> Este componente es para el login</h2>
+
+        <p>NOmbre dl usuario: {{ user }}</p>
+        <p>Contraseña: {{ password }}</p>
+        <button @click="login">Login</button>
+    </div>
+</template>
+
+<style scoped>
+.login-container {
+    border: 2px solid green;
+}
+</style>
